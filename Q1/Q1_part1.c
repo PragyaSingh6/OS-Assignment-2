689b5a727e1b20105140b7d6eb39dde37d2dace1
PragyaSingh6 <45223097+PragyaSingh6@users.noreply.github.com>
2020-10-28 22:48:28 +0530

    Update Q1_part1.c

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,3 +1,39 @@
 /* Name: Pragya Singh
    Roll_Number: 2017305 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int x=10;
+
+void func1()
+{
+	for(int i=11;i<=100;i++)
+	{
+		x++;
+		printf("%d\n",x);
+	}
+}
+
+void func2()
+{
+	for(int i=9;i>-90;i--)
+	{
+		x--;
+		printf("%d\n",x);
+	}
+}
+
+int main()
+{
+	if(fork()==0){
+		func2();
+			}
+	else{
+		wait(NULL);
+		func1();
+	}
+	return 0;
+}
 
